4187cfded1c8c6bc053c6792ac2395cb242ff6e7
JovinMtl <nsanzumukizath@gmail.com>
2024-01-26 22:42:10 +0200

    have added a super dynamic method for code generator

--- done1/app1/operations/code_transanction.py
+++ done1/app1/operations/code_transanction.py
@@ -13,14 +13,18 @@ class GenerateCode:
     
     we need to import that model of Codes"""
 
-    def __init__(self) -> None:
+    def __init__(self, high=12) -> None:
         self.input1 = [x for x in range(10)]
         self.input2 = ['a','A','b','B','c','C','d','D','e','E','f','F',
                        'j','J','k','K','l','L','m','M','n','o','O',
-                       'p','P','r','R','s','S','t','T',
+                       'p','P','r','R','s','S','t','T','u','U','v','V',
+                       'w','W','x','X','y','Y','z','Z',
                        ]
         self.choices = []
         self.code = ""
+        self.max = high
+    
+    
 
     def generate(self, table):
         worth = True
@@ -46,8 +50,30 @@ class GenerateCode:
                 else:
                     worth = True
     
+    def gene(self):
+        """This one is super dynamic"""
+        options = [1, 2, 3]
+        self.choices = []
+        inputs = [self.input1, self.input2]
+        
+        while len(self.choices) < self.max:
+            opt1 = choice(options)
+            input1 = choice(inputs)
+            for _ in range(opt1):
+                choice1 = choice(input1)
+                self.choices.append(choice1)
+            opt2 = choice(options)
+            input2 = choice(inputs)
+            for _ in range(opt2):
+                choice2 = choice(input2)
+                self.choices.append(choice2)
+        for element in self.choices[:self.max]:
+                self.code += str(element)
+        print(f"The length is {len(self.code)} / {self.max}")
+        return self.code
+    
 
 
 # jov = GenerateCode()
-# code = jov.generate()
+# code = jov.gene()
 # print(f"THe code generated is:\n{code}")
